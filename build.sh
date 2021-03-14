6cb04213ff69a1d47145e40c8ae28cdeceb8893e
Nishal Kulkarni <nishalkulkarni@gmail.com>
2021-03-14 18:41:29 +0530

    Added arcadia repo to build etc

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-WORK_DIR='/run/media/nishal/095a8bce-d904-4431-b12b-ebf1c9380d25/home/nishal/archiso_wd/'
-OUTPUT_DIR='/home/nishal/Documents/arcadia-builds/'
+WORK_DIR='/run/media/nishal/095a8bce-d904-4431-b12b-ebf1c9380d25/home/nishal/archiso_wd'
+OUTPUT_DIR='/home/nishal/Documents/arcadia-builds'
 PROFILE_DIR='/home/nishal/Documents/arcadia'
+
+cp "$PROFILE_DIR/pacman.conf" "$PROFILE_DIR/airootfs/etc"
 
 sudo rm -rf $WORK_DIR*
 
 sudo mkarchiso -v -w $WORK_DIR -o $OUTPUT_DIR $PROFILE_DIR
-
