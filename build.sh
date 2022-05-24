2a78f52bfc67f63ac435cae50ed3d5dab5266111
Gagan Malvi <malvigagan@gmail.com>
2022-05-24 15:01:48 +0200

    arcadia: fix licensing and lint.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
+#
+# Copyright (C) 2022 Arcadia Linux
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
 
-WORK_DIR='work'
-OUTPUT_DIR='output'
+echo "[-] Preparing build environment..."
+
+# Set directory names/paths
+WORK_DIR='tmp'
+OUTPUT_DIR='out'
 PROFILE_DIR='iso'
 
+# Create output and work directories.
 mkdir -p "$OUTPUT_DIR"
 mkdir -p "$WORK_DIR"
 
 cp "$PROFILE_DIR/pacman.conf" "$PROFILE_DIR/airootfs/etc"
 
-sudo rm -rf $WORK_DIR*
+# Cleanup step
+rm -rf $WORK_DIR
 
-sudo mkarchiso -v -w $WORK_DIR -o $OUTPUT_DIR $PROFILE_DIR
+echo "[-] Generating ISO..."
+# Generate ISO using mkarchiso
+mkarchiso -v -w $WORK_DIR -o $OUTPUT_DIR $PROFILE_DIR
